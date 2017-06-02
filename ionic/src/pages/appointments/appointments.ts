a4971b513cb03f3e150b3e88ad5ca0f92e4357d5
Jits Buijs <jitsbuijs@hotmail.com>
2017-06-02 12:53:52 +0200

    statis appointments page accordeon

--- ionic/src/pages/appointments/appointments.ts
+++ ionic/src/pages/appointments/appointments.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the AppointmentsPage page.
@@ -9,16 +9,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  */
 @IonicPage()
 @Component({
-  selector: 'page-appointments',
-  templateUrl: 'appointments.html',
+    selector: 'page-appointments',
+    templateUrl: 'appointments.html',
 })
 export class AppointmentsPage {
+    appointments: Array<{ title: string, value: string }>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.appointments = [
+            {title: 'appointment1', value: 'value1'},
+            {title: 'appointment2', value: 'value2'},
+            {title: 'appointment3', value: 'value3'}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AppointmentsPage');
-  }
+        ];
+    }
 
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad AppointmentsPage');
+    }
+
+    toggleActive(id) {
+        let classes = document.getElementById("icon" + id.toString()).className.split(" ");
+        console.log(classes);
+        for (let key in classes) {
+            if (classes[key] == "ion-md-arrow-dropdown") {
+                document.getElementById("item" + id.toString()).classList.add("hidden");
+                document.getElementById("icon" + id.toString()).classList.remove("ion-md-arrow-dropdown");
+                document.getElementById("icon" + id.toString()).classList.add("ion-md-arrow-dropright");
+                return ('');
+            }
+            for (let i = 0; i < this.appointments.length; i++) {
+                document.getElementById("item" + i.toString()).classList.add("hidden");
+                document.getElementById("icon" + i.toString()).classList.remove("ion-md-arrow-dropdown");
+                document.getElementById("icon" + i.toString()).classList.add("ion-md-arrow-dropright");
+            }
+            document.getElementById("item" + id.toString()).classList.remove("hidden");
+            document.getElementById("icon" + id.toString()).classList.remove("ion-md-arrow-dropright");
+            document.getElementById("icon" + id.toString()).classList.add("ion-md-arrow-dropdown");
+        }
+        
+    }
 }

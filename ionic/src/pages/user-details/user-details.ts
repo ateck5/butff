80a944b2cc01a23b8b9465882f7a7193403a6a6a
Jits Buijs <jitsbuijs@hotmail.com>
2017-06-06 08:57:26 +0200

    user-details less static

--- ionic/src/pages/user-details/user-details.ts
+++ ionic/src/pages/user-details/user-details.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the UserDetailsPage page.
@@ -9,16 +9,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  */
 @IonicPage()
 @Component({
-  selector: 'page-user-details',
-  templateUrl: 'user-details.html',
+    selector: 'page-user-details',
+    templateUrl: 'user-details.html',
 })
 export class UserDetailsPage {
+    user: { firstname: string, lastname: string, email: string, phone: string, phoneCountry?: string, nickname?: string, country?: string, city?: string, street?: string, streetNumber?: string, postcode?: string, discount?: string, discountDescription?: string };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.user = {firstname: "Herman", lastname: "Achternaamus", email: "ex@am.ple", phone: "0612345678"};
+        // this.user = {
+        //     firstname: "Herman",
+        //     lastname: "Achternaamus",
+        //     email: "ex@am.ple",
+        //     phone: "0612345678",
+        //     phoneCountry: "+31",
+        //     nickname: "Testus",
+        //     country: "The Netherlands",
+        //     city: "Breda",
+        //     street: "Straatstr",
+        //     streetNumber: "6A",
+        //     postcode: "1234AB",
+        //     discount: "999,00",
+        //     discountDescription: "€1,00 drink, €4,95 food€1,00 drink, €4,95 food€1,00 drink, €4,95 food€1,00 drink, €4,95 food€1,00 drink, €4,95 food€1,00 drink, €4,95 food€1,00 drink, €4,95 food€1,00 drink, €4,95 food€1,00 drink, €4,95 food"
+        // };
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserDetailsPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad UserDetailsPage');
+    }
 
 }
